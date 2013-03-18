d01ebb9aa226488ad6499d5cd2b587c4fddba7de
heryandi@gmail.com <heryandi@gmail.com@f96049ca-56e3-dfc1-16b3-7e3c9513fade>
2013-03-18 15:04:38 +0000

    Simplest filter

--- trunk/DOMXSSFilter/XSSFilter.js
+++ trunk/DOMXSSFilter/XSSFilter.js
@@ -1,15 +1,19 @@
 chrome.webRequest.onBeforeRequest.addListener (
+    function(details) {
+        var url = details.url;
+        if( url.indexOf("bing")!= -1) {
+            return {redirectUrl: "http://www.google.com"}; 
+        }
 
-          function(details) {
+        var newUrl = url.replace(/<script>.*<\/script>/g, "");
+        console.log("before replace: ", url);
+        console.log("after replace: ", newUrl);
+        return {redirectUrl: newUrl};
 
-                   var url = details.url;
-                  
-                   if( url.indexOf("bing")!= -1) {
-                          return {redirectUrl: "http://www.google.com"}; 
-                          
-                   } 
-                   return true;
-           },
-          {urls:["<all_urls>"]}, 
+        return {};
+    },
+    {
+        urls: ["<all_urls>"]
+    }, 
     // extraInfoSpec
-  ["blocking"]);
+    ["blocking"]);
