f8938aadbccfa033dc1c293b9de070aed8b11e89
heryandi@gmail.com <heryandi@gmail.com@f96049ca-56e3-dfc1-16b3-7e3c9513fade>
2013-03-20 14:49:56 +0000

    Force encodeURI

--- DOMXSSFilter/XSSFilter.js
+++ DOMXSSFilter/XSSFilter.js
@@ -1,17 +1,34 @@
+// Force encodeURI
 chrome.webRequest.onBeforeRequest.addListener (
     function(details) {
         var url = details.url;
+        /*
         if( url.indexOf("bing")!= -1) {
             return {redirectUrl: "http://www.google.com"}; 
         }
-
+        */
+        /*
         var newUrl = url.replace(/<script>.*<\/script>/g, "");
         console.log("before replace: ", url);
         console.log("after replace: ", newUrl);
         return {redirectUrl: newUrl};
+        */
+        
+        return {redirectUrl: encodeURI(details.url)};
     },
-    {
-        urls: ["<all_urls>"]
-    }, 
-    // extraInfoSpec
+    {urls: ["<all_urls>"]}, 
     ["blocking"]);
+
+// Remove all referer information
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        for (var i = 0; i < details.requestHeaders.length; ++i) {
+            if (details.requestHeaders[i].name === 'Referer') {
+                details.requestHeaders.splice(i, 1);
+                break;
+            }
+        }
+        return {requestHeaders: details.requestHeaders};
+    },
+    {urls: ["<all_urls>"]},
+    ["blocking", "requestHeaders"]);
