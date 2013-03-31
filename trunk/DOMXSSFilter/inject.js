28b324fe8bfd76418438e6425460780dfabd42f7
heryandi@gmail.com <heryandi@gmail.com@f96049ca-56e3-dfc1-16b3-7e3c9513fade>
2013-03-31 06:01:58 +0000

    Add content script

---
+++ trunk/DOMXSSFilter/inject.js
@@ -0,0 +1,7 @@
+console.log("inside inject.js");
+var aaa = document.createElement("div");
+aaa.innerHTML = "qqq";
+console.log(document.URL);
+console.log(document.location);
+//document.body.insertBefore(aaa, document.body.firstChild);
+//document.body.innerHTML = "ASDSADASD";
