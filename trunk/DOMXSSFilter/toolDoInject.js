479565276d2383d21a2707b22b8fba3d54caa2cb
heryandi@gmail.com <heryandi@gmail.com@f96049ca-56e3-dfc1-16b3-7e3c9513fade>
2013-04-03 14:12:32 +0000

    Another round of beautifying. Divide simple.html to 3 files.

--- trunk/DOMXSSFilter/toolDoInject.js
+++ trunk/DOMXSSFilter/toolDoInject.js
@@ -1,8 +1,8 @@
-var s = document.createElement('script');
+var s = document.createElement("script");
 //s.type = "application/javascript";
 s.src = chrome.extension.getURL("inject.js");
 s.onload = function() {
     this.parentNode.removeChild(this);
 };
 (document.head||document.documentElement).appendChild(s);
-console.log('Inject finished');
+console.log("Injection finished");
